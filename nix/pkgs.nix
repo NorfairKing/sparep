3dae77da4121ae99cb32faf1d918ae1412e3730d
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-06-13 21:58:45 +0200

    use niv

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -1,34 +1,14 @@
-let
-  pkgsv = import (import ./nixpkgs.nix);
-  pkgs = pkgsv {};
-  validity-overlay =
-    import (
-      pkgs.fetchFromGitHub (import ./validity-version.nix) + "/nix/overlay.nix"
-    );
-  cursor-overlay =
-    import (
-      pkgs.fetchFromGitHub (import ./cursor-version.nix) + "/nix/overlay.nix"
-    );
-  yamlparse-applicative-overlay =
-    import (
-      pkgs.fetchFromGitHub (import ./yamlparse-applicative-version.nix) + "/nix/overlay.nix"
-    );
-  appendful-overlay =
-    import (
-      pkgs.fetchFromGitHub (import ./appendful-version.nix) + "/nix/overlay.nix"
-    );
-  hastoryPkgs =
-    pkgsv {
-      overlays =
-        [
-          validity-overlay
-          cursor-overlay
-          yamlparse-applicative-overlay
-          appendful-overlay
-          (import ./gitignore-src.nix)
-          (import ./overlay.nix)
-        ];
-      config.allowUnfree = true;
-    };
-in
-hastoryPkgs
+{ sources ? import ./sources.nix
+}:
+import sources.nixpkgs {
+  overlays =
+    [
+      (final: previous: { inherit (import sources.gitignore { inherit (final) lib; }) gitignoreSource; })
+      (import (sources.appendful + "/nix/overlay.nix"))
+      (import (sources.cursor + "/nix/overlay.nix"))
+      (import (sources.yamlparse-applicative + "/nix/overlay.nix"))
+      (import (sources.validity + "/nix/overlay.nix"))
+      (import ./overlay.nix)
+    ];
+  config.allowUnfree = true;
+}
